de6ec6f078efdba648becf6ae71d10adc8010fdb
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-12-10 23:45:41 +0000

    Listen on a port with Rustigo

--- novanota-server/src/main.rs
+++ novanota-server/src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+use rustigo::prelude::*;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut rustigo = Rustigo::default();
+
+    Ok(rustigo.listen("0.0.0.0:2500", 4)?)
 }
